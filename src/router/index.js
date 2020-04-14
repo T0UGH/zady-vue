af9ef4f8f60a37c5b874d1d50c6bf846a02ad691
t0ugh <1243384249@qq.com>
2020-04-14 20:40:33 +0800

    fix: 删除多余的vue

--- src/router/index.js
+++ src/router/index.js
@@ -75,40 +75,6 @@ export const constantRoutes = [
         meta: { title: '新建项目', icon: 'form' }
       }
     ]
-  },
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
   }
 ]
 
@@ -142,66 +108,6 @@ export const asyncRoutes = [
       }
     ]
   },
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested',
-      meta: { title: 'Menu1-1' },
-      roles: ['master', 'owner']
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1', roles: ['master', 'owner'] },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1', roles: ['master', 'owner'] }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2', roles: ['master', 'owner'] },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1', roles: ['master', 'owner'] }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2', roles: ['master', 'owner'] }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3', roles: ['master', 'owner'] }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2', roles: ['master', 'owner'] }
-      }
-    ]
-  },
-
   {
     path: 'external-link',
     component: Layout,
---
+++ src/views/user/info/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <span>用户信息</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo'
+}
+</script>
+
+<style scoped>
+
+</style>
