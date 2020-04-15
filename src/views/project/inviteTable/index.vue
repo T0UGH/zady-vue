c6c571d020c9eaee859e835cb0f6197a1c0dd3e8
t0ugh <1243384249@qq.com>
2020-04-15 11:11:34 +0800

    feat: 添加几个svg图标

---
+++ src/views/project/inviteTable/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'InviteTable'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/project/userTable/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'UserTable'
+}
+</script>
+
+<style scoped>
+
+</style>
